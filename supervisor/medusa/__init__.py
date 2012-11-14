50e0be85256760f1193291ab1ef712ddb59f9706
Palm Kevin <palm.kevin@gmail.com>
2012-11-14 10:56:21 +0100

    add methods to string-module to simplify py3k porting of medusa

--- supervisor/medusa/__init__.py
+++ supervisor/medusa/__init__.py
@@ -4,3 +4,11 @@
 # created 2002/03/19, AMK
 
 __revision__ = "$Id: __init__.py,v 1.2 2002/03/19 22:49:34 amk Exp $"
+
+
+# The following methods are added to string module to simplify python3 portage
+import string
+string.split = lambda txt, sep=None, maxsplit=99999: txt.split(sep, maxsplit)
+string.join = lambda lst, sepChar="": sepChar.join(lst)
+string.atoi = lambda txt: int(txt)
+string.lower = lambda txt: txt.lower()
